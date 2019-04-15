61168ac02bf7fdf04ab339fdc47d55296d5d273f
SuichiM <MartiniRoqueEsteban@gmail.com>
2019-04-15 00:28:24 -0300

    modulo 2 solved

--- mod2_cmd_iterators.js
+++ mod2_cmd_iterators.js
@@ -1,1 +1,27 @@
 // Type your code here
+
+let [nodeRoute, fileRoute, ...params] = process.argv;
+
+// main
+console.log();
+console.log(`Route to Node.JS: ${nodeRoute}`);
+console.log(`Route to this file: ${fileRoute}`);
+
+//console.log(params);
+let excludeList = params.reduce((acc, el, idx, arr) => (el === '-r')? acc.concat(arr[idx+1]):acc, []);
+
+let filtrados = params.filter((el, idx, arr) => {
+    return el !== '-r' && excludeList.every(val => val !== el ) 
+});
+
+filtrados.sort().reduce(
+    (acc, elem, idx, arr)  => { 
+        if((elem !== arr[idx-1] && idx > 0) || idx === arr.length-1 ){
+            if( idx === arr.length-1) acc+=1;
+            console.log(`${arr[idx - 1]}: ${acc}`);
+            acc = 1;
+        }else{
+            acc++;
+        }
+        return acc;    
+    }, 0);
